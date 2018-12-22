1e40756b1e483e911818e213359cae674931972a
Conal Elliott <conal@conal.net>
2018-12-22 14:45:00 -0800

    rework examples

--- test/GoldTests.hs
+++ test/GoldTests.hs
@@ -6,7 +6,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
-{-# OPTIONS_GHC -Wall -Wno-unused-binds #-}
+{-# OPTIONS_GHC -Wall #-}
+-- {-# OPTIONS_GHC -Wno-unused-binds #-}
 
 module Main where
 
@@ -21,72 +22,36 @@ main :: IO ()
 main = do
   defaultMain basicTests
 
-gold :: Show a => String -> a -> TestTree
-gold nm = goldenVsString "basic"
-             ("test/gold/" <> nm <> ".txt")
-          . pure . pack . show
-
 basicTests :: TestTree
-basicTests = testGroup "various representations"
+basicTests = testGroup "Various representations"
   [ testGroup "" []
-  -- , testGroup "RegExp" [ tests @(RegExp Char) ]
-  -- , testGroup "LTrie"  [ tests @(LTrie Char Bool) ]
-  , gold "a" $ a
-  , gold "b" $ b
-  , gold "abcd" $ single @(LT Char) "abcd"
-  , gold "aOrb" $ aOrb
-  , gold "ab" $ ab
-  , gold "der-a-a" $ deriv 'a' a
-  , gold "der-ab-a" $ deriv 'a' ab
-
-  , gold "a-star-a"  $ accept ac "a"
-  , gold "a-star-aa" $ accept ac "aa"
-
-  , gold "a-star"    $ trim $ ac
-  , gold "aOrb-star" $ trim $ closure aOrb
-  , gold "ab-star"   $ trim $ closure ab
-
-  , gold "a-star-aaa"      $ accept ac "aaa"
-  , gold "a-star-aab"      $ accept ac "aab"
-  , gold "aOrb-star-aabba" $ accept (closure aOrb) "aabba"
-  , gold "aOrb-star-abab"  $ accept (closure aOrb) "abab"
-  , gold "aOrb-star-abac"  $ accept (closure aOrb) "abac"
-  , gold "ab-star-abab"    $ accept (closure ab) "abab"
-  , gold "ab-star-aabb"    $ accept (closure ab) "aabb"
-
+  , tests @(RegExp Char) "regexp"
+  , tests @(LT Char)     "lt"
   ]
- where
-  trim = trimLT @Char 7
-  a = single @(LT Char) "a"
-  b = single @(LT Char) "b"
-  aOrb = a <+> b
-  ab = a <.> b
-  ac = closure a
 
-tests :: forall z. Language z Char => TestTree
-tests = testGroup "basic tests"
+tests :: forall z. (Language z Char, Show z) => String -> TestTree
+tests group = testGroup group
   [ testGroup "" []
-#if 0
+
   , gold "a"                            $ a
   , gold "pig"                          $ pig
   , gold "pink-or-pig"                  $ pp
   , gold "derivs-pp-q"                  $ deriv 'q' pp
   , gold "derivs-pp-pi"                 $ derivs "pi" pp
   , gold "derivs-pp-pig"                $ derivs "pig" pp
-#endif
-  -- , gold "accept-pp-pi"                 $ app "pi"
-  -- , gold "accept-pp-pig"                $ app "pig"
-  -- , gold "accept-pp-pig"                $ app "pig"
-  -- , gold "accept-pp-pink"               $ app "pink"
-  -- , gold "accept-pp-ping"               $ app "ping"
 
-  -- , gold "accept-pps-q"               $ apps "q"
+  , gold "accept-pp-pi"                 $ app "pi"
+  , gold "accept-pp-pig"                $ app "pig"
+  , gold "accept-pp-pig"                $ app "pig"
+  , gold "accept-pp-pink"               $ app "pink"
+  , gold "accept-pp-ping"               $ app "ping"
 
-  -- , gold "accept-pps-pig"               $ apps "pig"
-  -- , gold "accept-pps-pigpig"            $ apps "pigpig"
-  -- , gold "accept-pps-pigping"           $ apps "pigping"
-  -- , gold "accept-pps-pinkpigpinkpigpig" $ apps "pinkpigpinkpigpig"
-#if 1
+  , gold "accept-pps-q"                 $ apps "q"
+  , gold "accept-pps-pig"               $ apps "pig"
+  , gold "accept-pps-pigpig"            $ apps "pigpig"
+  , gold "accept-pps-pigping"           $ apps "pigping"
+  , gold "accept-pps-pinkpigpinkpigpig" $ apps "pinkpigpinkpigpig"
+
   -- OptimizeRegexp in Code.hs causes these recursive examples to diverge.
   , gold "accept-anbn-eps"              $ ranbn ""
   , gold "accept-anbn-ab"               $ ranbn "ab"
@@ -95,8 +60,7 @@ tests = testGroup "basic tests"
   , gold "accept-anbn-aacbb"            $ ranbn "aacbb"
   , gold "accept-anbn-aaabbb"           $ ranbn "aaabbb"
   , gold "accept-anbn-aaabbbb"          $ ranbn "aaabbbb"
-  -- , gold "pink-and-pig"              $ pink `intersectB` pig
-#endif
+
   ]
  where
    sing = single @z
@@ -110,3 +74,7 @@ tests = testGroup "basic tests"
    apps  = accept pps
    anbn  = one <+> (a <.> anbn <.> b)
    ranbn = accept anbn
+   gold :: Show a => String -> a -> TestTree
+   gold nm = goldenVsString "basic"
+                ("test/gold/" <> group <> "/" <> nm <> ".txt")
+             . pure . pack . show

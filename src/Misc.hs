2c4f80e295caff3bfa8610e18334935b140d70d7
Conal Elliott <conal@conal.net>
2019-02-05 21:12:26 -0800

    Stream

--- src/Misc.hs
+++ src/Misc.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE CPP #-}
+
 -- {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
 
 -- | Miscellany
 
 module Misc where
+
+import Control.Applicative (liftA2)
 
 infixl 7 :*
 infixl 6 :+
@@ -14,3 +18,18 @@ type Unop a = a -> a
 
 bool :: a -> a -> Bool -> a
 bool t e b = if b then t else e
+
+
+infixr 1 :#
+data Stream b = b :# Stream b
+
+instance Functor Stream where fmap f (b :# bs) = f b :# fmap f bs
+
+instance Applicative Stream where
+#if 1
+  pure b = q where q = b :# q
+  liftA2 h = go where go (a :# as) (b :# bs) = h a b :# go as bs
+#else
+  pure b = b :# pure b
+  liftA2 h (a :# as) (b :# bs) = h a b :# liftA2 h as bs
+#endif

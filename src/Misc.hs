2341c20c1210f2f84f7564dfa89b57224067c01c
Conal Elliott <conal@conal.net>
2019-02-06 15:03:17 -0800

    move streams to Stream

--- src/Misc.hs
+++ src/Misc.hs
@@ -6,9 +6,6 @@
 
 module Misc where
 
-import Control.Applicative (liftA2)
-
-infixl 7 :*
 infixl 6 :+
 
 type (:*)  = (,)
@@ -20,25 +17,3 @@ bool :: a -> a -> Bool -> a
 bool t e b = if b then t else e
 
 
--- TODO: Maybe move Stream to its own module, importing Constrained
-
-infixr 1 :#
-data Stream b = b :# Stream b
-
-instance Functor Stream where fmap f (b :# bs) = f b :# fmap f bs
-
-instance Applicative Stream where
-#if 1
-  pure b = q where q = b :# q
-  liftA2 h = go where go (a :# as) (b :# bs) = h a b :# go as bs
-#else
-  pure b = b :# pure b
-  liftA2 h (a :# as) (b :# bs) = h a b :# liftA2 h as bs
-#endif
-
-instance Monad Stream where
-  ss >>= f = joinS (fmap f ss)
-  -- (s :# ss') >>= f = let (b :# _) = f s in b :# (ss' >>= f)
-
-joinS :: Stream (Stream a) -> Stream a
-joinS ((a :# _) :# ss') = a :# joinS ss'

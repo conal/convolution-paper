1760a7aa83430f5c0315bcb8547974fef493992c
Conal Elliott <conal@conal.net>
2019-02-07 21:02:17 -0800

    Examples

---
+++ src/Examples.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE RankNTypes #-}
+
+-- {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
+
+-- | Some polymorphic language examples
+
+module Examples where
+
+import Semi
+import Language
+
+sa, sb, pink, pig :: (HasSingle String b x, Semiring b) => x
+sa   = single "a"
+sb   = single "b"
+pink = single "pink"
+pig  = single "pig"
+
+pp :: (HasSingle String b x, Semiring b, Additive x) => x
+pp   = pink <+> pig
+
+as, ass, pps :: (HasSingle String b x, Semiring b, StarSemiring x) => x
+as  = star sa
+ass = star as
+pps = star pp
+
+anbn :: (HasSingle String b x, Semiring b, Semiring x) => x
+anbn  = one <+> (sa <.> anbn <.> sb)

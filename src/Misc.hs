bd311a2e839f168b089e766a295730b7d7ee22c6
Conal Elliott <conal@conal.net>
2019-02-16 19:46:54 -0800

    Con1, Con2

--- src/Misc.hs
+++ src/Misc.hs
@@ -13,3 +13,11 @@ type Unop a = a -> a
 
 bool :: a -> a -> Bool -> a
 bool t e b = if b then t else e
+
+-- | Handy universal constraint alias
+class    (forall u. con u => con (h u)) => Con1 con h
+instance (forall u. con u => con (h u)) => Con1 con h
+
+-- | Handy universal constraint alias
+class    (forall u v. con u v => con (h u) (h v)) => Con2 con h
+instance (forall u v. con u v => con (h u) (h v)) => Con2 con h

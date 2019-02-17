e89d8c6c3063a24509c7dc1dcde492dc3d1de6c1
Conal Elliott <conal@conal.net>
2019-02-16 19:43:36 -0800

    Now list trie using functions instead of maps

--- src/Decomp.hs
+++ src/Decomp.hs
@@ -1,82 +1,66 @@
+#ifdef EXAMPLES
+{-# OPTIONS_GHC -Wno-unused-imports #-} -- Examples
+#endif
 
-{-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
+-- | List tries using functions from characters
 
--- | Decomposable as a "language" representation
+module Decomp where  -- TODO: rename module
 
-module Decomp where
+import Prelude hiding (sum,product)
 
-import Text.Show.Functions ()  -- for Decomp
-
-import Constrained
 import Semi
-import Language
 
-#include "GenInstances.inc"
+#ifdef EXAMPLES
+import Examples
+#endif
 
+-- | List trie, denoting '[c] -> b'"
 infix 1 :<
-data Decomp c s = s :< (c -> Decomp c s) deriving Show
+data LTrie c b = b :< (c -> LTrie c b)
 
-instance Functor (Decomp c) where
-  fmap f = go where go (a :< dp) = f a :< go . dp
-
--- TODO: define fmapD for arbitrary decomposables
-
-instance FunctorC (Decomp c)
-
--- trieFun :: (Ord c, Additive b) => Decomp c b -> ([c] -> b)
--- trieFun (a :< dp) = a <: trieFun . (dp !)
-
-instance (Ord c, Additive b) => Indexable [c] b (Decomp c b) where
-  (!) (a :< dp) = a <: (!) . (dp !)
-
-  -- (a :< _ ) ! [] = a
-  -- (_ :< dp) ! (c:cs) = (dp ! c) ! cs
-
-  -- (a :< _ ) ! [] = a
-  -- (_ :< dp) ! (c:cs) = dp c ! cs
-
--- TODO: define (!) for arbitrary decomposables.
-
-instance (Ord c, Additive b) => Additive (Decomp c b) where
-  zero = zero :< zero
-  (a :< dp) <+> (b :< dq) = a <+> b  :<  dp <+> dq
-
-FunctorSemimodule(Decomp c)
-
-instance (Ord c, Additive b) => HasSingle [c] b (Decomp c b) where
-  w +-> b = foldr cons nil w
-   where
-     nil = b :< zero
-     cons c x = zero :< (c +-> x)
-
--- Is HasSingle even useful on Decomp?
-
-instance (DetectableZero b, Eq c) => Decomposable b ((->) c) (Decomp c b) where
+instance Decomposable b ((->) c) (LTrie c b) where
   (<:) = (:<)
-  atEps (a :< _) = a
-  deriv (_ :< d) = d
+  decomp (b :< dp) = (b, dp)
 
-type Decomp' c b = Convo (Decomp c b)
+#ifdef EXAMPLES
 
--- >>> let a = single "a" :: Decomp Char Bool
--- >>> a
--- False :< <function>
--- >>> deriv a ! 'a'
--- True :< <function>
--- >>> a ! ""
+{--------------------------------------------------------------------
+    Examples
+--------------------------------------------------------------------}
+
+type L  = LTrie  Char Bool
+type L' = Decomp L
+
+xpig,xpink,xpp,xanbn :: L'
+xpig = pig
+xpink = pink
+xpp = pp
+xanbn = anbn
+
+-- >>> xpig ! "p"
 -- False
--- >>> a ! "a"
+-- >>> xpig ! "pig"
 -- True
--- >>> a ! "b"
+-- >>> xpig ! "piggy"
 -- False
--- >>> a ! "ab"
+
+-- >>> xpp ! "pink"
+-- True
+-- >>> xpp ! "pig"
+-- True
+-- >>> xpp ! "ping"
 -- False
---
--- >>> let a' = C a
--- >>> a'
--- C (False :< <function>)
--- >>> a' <+> a'
--- C (False :< <function>)
--- >>> one :: Decomp' Char Bool
+
+-- >>> xanbn ! ""
+-- True
+-- >>> xanbn ! "a"
+-- False
+-- >>> xanbn ! "ab"
+-- True
+-- >>> xanbn ! "aabb"
+-- True
+-- >>> xanbn ! "aaaaabbbbb"
+-- True
+
+#endif
 

a83144124e06ca4b361d1be083707d837f9b3fcf
Conal Elliott <conal@conal.net>
2019-02-23 15:58:29 -0800

    PolyF (untested); PolyM better

--- src/Poly.hs
+++ src/Poly.hs
@@ -9,7 +9,8 @@ import Prelude hiding ((^),sum)
 import GHC.Natural (Natural)
 -- import Data.Map (Map)
 -- import qualified Data.Map as M
-import Data.List (intercalate,intersperse)
+import Data.List (intercalate,intersperse,sortOn)
+import qualified Data.Foldable as F
 
 import Misc ((:*))
 import Semi
@@ -83,11 +84,11 @@ showsPrecTerm names d (b,m) = showParen (d >= 7) $
     where
       name = showsPrec 9 (M.findWithDefault "?" k names)
 
--- newtype Poly n b = Poly (Map (Map n N) b) deriving
+-- newtype PolyM n b = PolyM (Map (Map n N) b) deriving
 --   (Additive, Semiring, Functor, Indexable n, HasSingle n)
 
--- instance (DetectableOne b, Show b) => Show (Poly n b) where
---   showsPrec d (Poly m) = showParen (d >= 6) $
+-- instance (DetectableOne b, Show b) => Show (PolyM n b) where
+--   showsPrec d (PolyM m) = showParen (d >= 6) $
 --      foldr (.) id (intersperse (showString " + ") (term <$> M.toDescList m))
 --    where
 --      term :: (N,b) -> ShowS
@@ -104,12 +105,14 @@ showsPrecTerm names d (b,m) = showParen (d >= 7) $
 #endif
 
 -- Polynomials with named "variables"
-newtype Poly b = Poly { unPoly :: Map (Map String N) b } deriving
+newtype PolyM b = PolyM { unPolyM :: Map (Map String N) b } deriving
   (Additive, Semiring, Functor, Indexable n, HasSingle n)
 
-instance (DetectableOne b, Show b) => Show (Poly b) where
-  showsPrec d (Poly m) = showParen (d >= 6) $
-     foldr (.) id (intersperse (showString " + ") (term <$> M.toDescList m))
+instance (DetectableOne b, Show b) => Show (PolyM b) where
+  showsPrec d (PolyM m) = showParen (d >= 6) $
+     foldr (.) id (intersperse (showString " + ")
+                    -- TODO: better ordering
+                    (term <$> sortOn (M.keys . fst) (M.toDescList m)))
    where
      term :: (Map String N,b) -> ShowS
      term (pows, b)
@@ -123,16 +126,67 @@ instance (DetectableOne b, Show b) => Show (Poly b) where
           | i == 1    = showString name
           | otherwise = showString name . showString "^" . showsPrec 8 i
 
+-- m :: Map (Map String N) b
+-- M.toList m :: [(Map String N, b)]
+
+-- fst :: (Map String N, b) -> Map String N
+-- keys . fst
+
+-- M.toList m :: [(Map String N, b)]
+
 -- TODO: try changing isZero for Map to be 'all isZero'. Might wedge on recursive examples.
 
-x,y :: Poly Int
-x = single (single "x")
-y = single (single "y")
-
+-- >>> x = single (single "x") :: PolyM Int
+-- >>> y = single (single "y") :: PolyM Int
 -- >>> x <+> y
--- y + x
+-- x + y
 -- >>> (x <+> y)^2
--- y^2 + x^2 + 2xy
+-- x^2 + 2xy + y^2
 -- >>> (x <+> y)^5
--- y^5 + x^5 + 5x^4y + 10x^3y^2 + 10x^2y^3 + 5xy^4
+-- x^5 + 5x^4y + 10x^3y^2 + 10x^2y^3 + 5xy^4 + y^5
+
+-- >>> x = single (single "x") :: PolyM Int
+-- >>> y = single (single "y") :: PolyM Int
+-- >>> z = single (single "z") :: PolyM Int
+-- >>> x <+> y <+> z
+-- x + y + z
+-- >>> (x <+> y <+> z)^2
+-- x^2 + 2xy + 2xz + y^2 + 2yz + z^2
+-- >>> (x <+> y <+> z)^5
+-- x^5 + 5x^4y + 10x^3y^2 + 10x^2y^3 + 5xy^4 + 20x^3yz + 30x^2y^2z + 30x^2yz^2 + 20xy^3z + 30xy^2z^2 + 20xyz^3 + 5x^4z + 10x^3z^2 + 10x^2z^3 + 5xz^4 + y^5 + 5y^4z + 10y^3z^2 + 10y^2z^3 + 5yz^4 + z^5
+
+
+newtype PolyF h b = PolyF { unPolyF :: Map (h N) b } deriving Functor
+
+deriving instance (Ord (h N), Additive b) => Additive (PolyF h b)
+deriving instance (Ord (h N), Monoid (h N), Semiring b) => Semiring (PolyF h b)
+deriving instance (Ord (h N), Additive k) => Indexable k (PolyF h)
+deriving instance (Ord (h N), Additive k) => HasSingle k (PolyF h)
+
+instance (Foldable h, DetectableOne b, Show b) => Show (PolyF h b) where
+  showsPrec d (PolyF m) = showParen (d >= 6) $
+     foldr (.) id (intersperse (showString " + ") (term <$> M.toDescList m))
+   where
+     term :: (h N,b) -> ShowS
+     term (pows, b)
+       | all (== 0) pows = showsPrec 6 b
+       | isOne b = xs
+       | otherwise = showsPrec 6 b . {- showString "*" . -} xs
+      where
+        xs :: ShowS
+        xs = foldr (.) id (factor <$> F.toList pows)
+        factor (Sum i)
+          | i == 0    = id
+          | i == 1    = showString name
+          | otherwise = showString name . showString "^" . showsPrec 8 i
+        name = "?"
+
+-- >>> x = single (single "x") :: PolyF (Map String) Int
+-- >>> y = single (single "y") :: PolyF (Map String) Int
+-- >>> x <+> y
+-- ? + ?
+-- >>> (x <+> y)^2
+-- ?^2 + ?^2 + 2??
+-- >>> (x <+> y)^5
+-- ?^5 + ?^5 + 5?^4? + 10?^3?^2 + 10?^2?^3 + 5??^4
 

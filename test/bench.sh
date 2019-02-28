e4db1ae70e5c7de6ff97d555e68c446f1cfda9b5
Conal Elliott <conal@conal.net>
2019-02-28 13:38:28 -0800

    looking good

--- test/bench.sh
+++ test/bench.sh
@@ -1,6 +1,7 @@
-# stack bench 2>&1 > o
+stack bench >& o
+
 echo -n "\\stats{"
 grep Group o | cut -c10-100 | sed 's/"/}{/' | tr -d '\n'
-grep time o | cut -c22-29 | tr '\n' "#" | sed 's/#/}{/g'
+grep time o | cut -c22-29 | tr '\n' "#" | sed 's/#/}{/g' | tr -d '\n'
 echo "}"
 

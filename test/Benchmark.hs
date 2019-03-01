8f644112f39817cd8b70c01787ad26ca431c5c30
Conal Elliott <conal@conal.net>
2019-02-28 16:01:46 -0800

    benchmarking

--- test/Benchmark.hs
+++ test/Benchmark.hs
@@ -19,90 +19,65 @@ import Examples
 
 main :: IO ()
 main = do
-  putChar '\n'
-  createDirectoryIfMissing True outDir
 
-  -- group "star-a" (star (single "a")) []
-  --   [ ("a100", replicate 100 'a') ]
+  defaultMainWith config [
+      bgroup "" []
 
-  -- -- group "starR-a" (starR (single "a")) [] [("a50", replicate 50 'a')]
-  -- -- group "starL-a" (starL (single "a")) [] [("a50", replicate 50 'a')]
+    , group "star-a" (star (single "a")) [] $
+        replicate 100 'a'
 
-  -- group "letters" (star letter) []
-  --   [ ("asdf-50", cats 50 "asdf") ]
+    , group "letters" (star letter) [] $
+        cats 50 "asdf" 
 
-  group "fish" (star letter <.> single "fish" <.> star letter) [] $
-    []
-    ++ [("30", take 30 (cycle az) ++ "fish" ++ take 30 (cycle az))]
+    , group "fish" (star letter <.> single "fish" <.> star letter) [] $
+        take 30 (cycle az) ++ "fish" ++ take 30 (cycle az)
 
-  -- group "asas" (star (single "a") <.> star (single "a")) [] $
-  --   []
-  --   -- ++ lit <$> [ "aabbb", "aabbba", "aaaa" , "bb" ]
-  --   ++ [("a30b30",replicate 30 'a' ++ replicate 30 'a')]
-  --   -- ++ [("a30cb30",replicate 30 'a' ++ "c" ++ replicate 30 'a')]
+    , group "asas" (star (single "a") <.> star (single "a")) [] $
+        replicate 30 'a' ++ replicate 30 'a'
 
-  -- group "asbs" (star (single "a") <.> star (single "b")) [] $
-  --   []
-  --   -- ++ lit <$> [ "aabbb", "aabbba", "aaaa" , "bb" ]
-  --   ++ [("a30b30",replicate 30 'a' ++ replicate 30 'b')]
-  --   -- ++ [("a30cb30",replicate 30 'a' ++ "c" ++ replicate 30 'b')]
+    , group "asbs" (star (single "a") <.> star (single "b")) [] $
+        replicate 30 'a' ++ replicate 30 'b'
 
-  -- group "asbas" (star (single "a") <.> single "b" <.> star (single "a")) [] $
-  --   []
-  --   -- ++ lit <$> [ "aabaaa", "aabba", "aaaa" , "bb" ]
-  --   ++ [("a30ba30",replicate 30 'a' ++ "b" ++ replicate 30 'a')]
+    , group "asbas" (star (single "a") <.> single "b" <.> star (single "a")) [] $
+        replicate 30 'a' ++ "b" ++ replicate 30 'a'
 
-  -- -- With O = N, the dyck examples don't work for RegExp:Function, while anbn
-  -- -- is okay.
-  -- group "dyck" dyck ["RegExp:Map","RegExp:IntMap"] $
-  --   []
-  --   -- ++ lit <$> [ "[]","[[]]","[[a]]","[[]][]" ]
-  --   ++ [("", "[[[[[[[]]][[[[]]]][[[]]][]][[[[]]]]]][]]")]
+      -- With O = N, the dyck examples don't work for RegExp:Function, while anbn
+      -- is okay.
+    , group "dyck" dyck ["RegExp:Map","RegExp:IntMap"] $
+        "[[[[[[[]]][[[[]]]][[[]]][]][[[[]]]]]][]]"
 
-  -- group "anbn" anbn ["RegExp:Map","RegExp:IntMap"] $
-  --   []
-  --   -- ++ map lit ["","ab","aacbb","aaabbb","aaabbbb"]
-  --   ++ [("30",replicate 30 'a' ++ replicate 30 'b')]
+    , group "anbn" anbn ["RegExp:Map","RegExp:IntMap"] $
+        replicate 30 'a' ++ replicate 30 'b'
 
-lit :: String -> (String,String)
-lit str = (show str,str)
-
-outDir :: FilePath
-outDir = "test/Benchmarks"
+    ]
+ where
+   config = defaultConfig
+     { 
+      timeLimit = 5 -- 0.1
+     }
 
 type Ok x b = (HasSingle String b x, StarSemiring x, StarSemiring b, NFData b)
 
-group :: String -> (forall x b. Ok x b => x) -> [String] -> [(String,String)] -> IO ()
-group groupName example omit dats =
-  do putStrLn ("# Group " ++ show groupName ++ "\n\n```\n")
-     defaultMainWith config (dat <$> dats)
-     putStrLn "```\n"
+group :: String -> (forall x b. Ok x b => x) -> [String] -> String -> Benchmark
+group groupName example omit str =
+  bgroup groupName
+    [ bgroup "" []
+
+    , style @(RegExp ((->)   Char) O) "RegExp:Function"
+    , style @(RegExp (Map    Char) O) "RegExp:Map"
+    -- , style @(RegExp CharMap       O) "RegExp:IntMap"
+
+    , style @(Cofree  ((->)   Char) O) "Cofree:Function"
+    , style @(Cofree  (Map    Char) O) "Cofree:Map"
+    -- , style @(Cofree  CharMap       O) "Cofree:IntMap"
+
+    ]
  where
-   config = defaultConfig
-     { reportFile = Just (outDir ++ "/" ++ groupName ++ ".html")
-     -- , csvFile    = Just (outDir ++ "/" ++ groupName ++ ".csv")
-     , timeLimit  = 5 -- 1
-     -- , timeLimit = 0, resamples = 1
-     }
-   dat :: (String,String) -> Benchmark
-   dat (dname,str) =
-     bgroup dname
-       [ bgroup "" []
+   style :: forall x b. Ok x b => String -> Benchmark
+   style s | s `elem` omit = bench s (whnf id ())
+                             -- bgroup "" []
+           | otherwise     = bench s (nf (example @x @b !) str)
 
-       , style @(RegExp ((->)   Char) O) "RegExp:Function"
-       , style @(RegExp (Map    Char) O) "RegExp:Map"
-       -- , style @(RegExp CharMap       O) "RegExp:IntMap"
-
-       , style @(Cofree  ((->)   Char) O) "Cofree:Function"
-       , style @(Cofree  (Map    Char) O) "Cofree:Map"
-       -- , style @(Cofree  CharMap       O) "Cofree:IntMap"
-
-       ]
-    where
-      style :: forall x b. Ok x b => String -> Benchmark
-      style s | s `elem` omit = bench s (whnf id ())
-                                -- bgroup "" []
-              | otherwise     = bench s (nf (example @x @b !) str)
 
 type O = Bool -- N
 
